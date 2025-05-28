7e1c05bdad5f77c6c395a9403865975780a5e1ab
Kat Leight <leightkt@gmail.com>
2025-05-27 20:12:25 -0600

    tweak countdown

--- game.py
+++ game.py
@@ -101,7 +101,7 @@ while running:
     if fortune_start_time is not None:
         GPIO.output(LED_GPIO, GPIO.LOW)
         current_time = pygame.time.get_ticks()
-        remaining_time = max(0, ((fortune_duration - (current_time - fortune_start_time)) // 1000) - 1)
+        remaining_time = max(1, (fortune_duration - (current_time - fortune_start_time)) // 1000)
         # Show fortune screen with the saved card
         tell_fortune(fortune_card_image, fortune_card_title, meaning_text, fortune, screen, small_font, font, bg, dark_overlay)
 
